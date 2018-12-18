2a268c453e1ae0692a776dcf046fe806620a76fe
lp <1131352900@qq.com>
2018-12-18 21:12:28 +0800

    first commit

--- push.bat
+++ push.bat
@@ -1,5 +1,8 @@
+cd %~dp0
 git init
 git add .
-git status
-git commit -m "Ã·Ωª"
+git commit -m "first commit"
+git remote rm origin
+git remote add origin git@github.com:liupeng2017/python.git
+git push -u origin master
 pause
